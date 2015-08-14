88829dad7e5e8c5bd6e2dc042dcbd4d986cf8aa8
Jan Pospisil <j.pospisil@cern.ch>
2015-08-14 15:29:36 +0200

    fixed background color change in "CTP counters" window

--- v/vmeb/counters.py
+++ v/vmeb/counters.py
@@ -1350,7 +1350,7 @@ class VMEcnts(CTPcnts):
         else:                   # CTP: accrual valid
           newval= self.allregs[cntnum]
           newbin= eval(self.allregs[cntnum])
-        if prevent != newval:
+        if eval(prevent) != eval(newval):
           self.regs[i].cntentry.setColor(myw.COLOR_VALCHANGED)
           self.regs[i].cntentry.setEntry(newval)
         else:
